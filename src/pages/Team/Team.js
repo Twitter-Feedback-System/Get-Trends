7dbb52fe4d74b98f329db5cfd96be1e97b94b86a
Vaspar-a <vaspudie@gmail.com>
2021-02-25 10:29:39 +0530

    Cards Sorted. Sorted Header. Changed font-size

--- src/pages/Team/Team.js
+++ src/pages/Team/Team.js
@@ -1,20 +1,22 @@
 import React, { Component } from 'react'
 import './Team.css'
-// import ali from '../../assets/Profile/Ali.jpg'
+import imgAli from '../../assets/Profile/Ali.jpg'
+import imgNikunj from '../../assets/Profile/Nikunj.jpg'
+import imgVaspar from '../../assets/Profile/Vaspar.jpg'
+import imgRishabh from '../../assets/Profile/Rishabh.jpg'
+import imgVidit from '../../assets/Profile/Vidit.jpg'
 
-// import '../../../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import Profileurl from '../../assets/Profile/profile-bg.jpg'
  class Team extends Component {
-   
     constructor(props) {
-        super(props) 
+        super(props);
         this.state = { 
            profile : [
-              { id: 1, Name: 'Ali', Role: 'Backend Developer', Imgurl: ('../../assets/Profile/Ali.jpg'), Giturl: "https://github.com/Aliabbas78692", About: " aeeyhezr erhj ez ezj eruez u ksgbskjb ukhbjk esgksngsje " },
-              { id: 2, Name: 'Nikunj', Role: 'Frontend Developer', Imgurl: '../../assets/Profile/Nikunj.jpg', Giturl: "https://github.com/Nikunj018", About: " aeeyhezr erhj ez ezj eruez u ksgbskjb ukhbjk esgksngsje " },
-              { id: 3, Name: 'Vaspar', Role: 'Full stack Developer', Imgurl: '../../assets/Profile/Vaspar.jpg', Giturl: "https://github.com/Vaspar-a", About: " Haeeyhezr erhj ez ezj eruez u ksgbskjb ukhbjk esgksngsje " },
-              { id: 4, Name: 'Rishabh', Role: 'Frontend Developer', Imgurl: '../../assets/Profile/Rishabh.jpg', Giturl: "https://github.com/Rain1213", About: " aeeyhezr erhj ez ezj eruez u ksgbskjb ukhbjk esgksngsje" },
-              { id: 5, Name: 'Vidit', Role: 'Backend Developer', Imgurl: '../../assets/Profile/Vidit.jpg', Giturl: "https://github.com/Vidit631", About: "aeeyhezr erhj ez ezj eruez u ksgbskjb ukhbjk esgksngsje" }
+              { id: 1, Name: 'Ali', Role: 'Backend Developer', Imgurl: imgAli, Giturl: "https://github.com/Aliabbas78692", About: " aeeyhezr erhj ez ezj eruez u ksgbskjb ukhbjk esgksngsje " },
+              { id: 2, Name: 'Nikunj', Role: 'Frontend Developer', Imgurl: imgNikunj, Giturl: "https://github.com/Nikunj018", About: " aeeyhezr erhj ez ezj eruez u ksgbskjb ukhbjk esgksngsje " },
+              { id: 3, Name: 'Vaspar', Role: 'Full stack Developer', Imgurl: imgVaspar, Giturl: "https://github.com/Vaspar-a", About: " Haeeyhezr erhj ez ezj eruez u ksgbskjb ukhbjk esgksngsje " },
+              { id: 4, Name: 'Rishabh', Role: 'Frontend Developer', Imgurl: imgRishabh, Giturl: "https://github.com/Rain1213", About: " aeeyhezr erhj ez ezj eruez u ksgbskjb ukhbjk esgksngsje" },
+              { id: 5, Name: 'Vidit', Role: 'Backend Developer', Imgurl: imgVidit, Giturl: "https://github.com/Vidit631", About: "aeeyhezr erhj ez ezj eruez u ksgbskjb ukhbjk esgksngsje" }
            ]
         }
      }
@@ -31,8 +33,8 @@ import Profileurl from '../../assets/Profile/profile-bg.jpg'
                         {this.state.profile.map((item,index)=>{
                         return(
                             <div key = {index}className = "card">
-                                <img src = {Profileurl} className = "card-img"></img>
-                                <img src = {item.Imgurl} className = "profile-img"></img>
+                                <img src = {Profileurl} className = "card-img" alt="card-img"></img>
+                                <img src = {item.Imgurl} className = "profile-img" alt="profile-img"></img>
                                 <h1>{item.Name}</h1>
                                 <p className= "role">{item.Role}</p>
                                 <p className= "about">
